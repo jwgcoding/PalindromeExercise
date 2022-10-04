e8b380f68aa360ffc72fa6c7ee5586fa78911a66
Gary Li <garyli0802@yahoo.com>
2022-10-04 12:07:24 -0700

    initial commit

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+﻿using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,27 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar",true)]
+        [InlineData("wow",true)]
+        [InlineData("mom",true)]
+        [InlineData("radar",true)]
+        [InlineData("dad",true)]
+        [InlineData("gary",false)]
+        [InlineData("yellow",false)]
+        [InlineData("",false)]
+        [InlineData("Racecar",true)]// ← you will need to put some test data and expected result here.  
+        // For example [InlineData(“racecar”, true)]
+        public void IsAPalindrome_Test(string word, bool expected)
         {
-
+             
+            // Arrange
+            var ws = new WordSmith();
+            // Act
+            var actual = ws.IsAPalindrome(word);
+            // Assert 
+            Assert.Equal(expected, actual);
         }
+
     }
 }
